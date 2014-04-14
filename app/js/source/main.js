f0221c174afacfe897a7a3c119d14b8f26268853
Chyld Medford <chyld.medford@gmail.com>
2014-04-14 12:02:37 -0500

    finshed

--- app/js/source/main.js
+++ app/js/source/main.js
@@ -9,6 +9,72 @@
     $('.clear').click(clear);
     $('.decimal').click(decimal);
     $('.sign').click(sign);
+    $('.push').click(push);
+    $('.operator').click(operator);
+  }
+
+  function operator(){
+    var op = $(this).data('op');
+    var x = $('#queue > div:nth-child(1)').text() * 1;
+    var y = $('#queue > div:nth-child(2)').text() * 1;
+    var result;
+
+    switch(op){
+      case 'add':
+        result = x + y;
+        break;
+      case 'sub':
+        result = x - y;
+        break;
+      case 'mul':
+        result = x * y;
+        break;
+      case 'div':
+        result = x / y;
+        break;
+      case 'exp':
+        result = Math.pow(x, y);
+        break;
+      case 'root':
+        result = Math.sqrt(x);
+        break;
+      case 'fact':
+        result = factorial(x);
+        break;
+      case 'sum':
+        result = sum();
+    }
+
+    $('#display').text(result);
+  }
+
+  function sum(){
+    var total = 0;
+
+    $('#queue > div').each(function(index, div){
+      var x = div.textContent * 1;
+      total += x;
+    });
+
+    return total;
+  }
+
+  function factorial(x){
+    var product = 1;
+
+    for(var i = 2; i <= x; i++){
+      product *= i;
+    }
+
+    return product;
+  }
+
+  function push(){
+    var display = $('#display').text();
+    $('#display').text(0);
+    var $div = $('<div>');
+    $div.text(display);
+    $('#queue').prepend($div);
   }
 
   function title(){
@@ -39,6 +105,8 @@
 
     if(type === 'C'){
       $('#display').text(0);
+    }else{
+      $('#queue').empty();
     }
   }
 
